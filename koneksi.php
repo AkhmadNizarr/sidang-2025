88ed5291e00be26b7aadc85b47da9dcc5cc5863e
trianggautama <trianggategarutama@gmail.com>
2025-08-23 11:28:53 +0800

    ubah koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,16 +1,2 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "root"; // MAMP's default MySQL root password is 'root'
-$database = "sidang"; // Replace with your actual database name
-$port = 8889; // MAMP's default MySQL port
-
-// Establish the connection
-$koneksi = mysqli_connect($host, $username, $password, $database, $port);
-
-// Check for connection errors
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-}
-?>
+$koneksi = mysqli_connect("localhost", "root", "", "kantor");
